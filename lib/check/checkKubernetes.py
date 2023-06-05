3b7714e9984ce503a3d73f98baa5ac83a3ba6287
Koos85 <koosjoosten@gmail.com>
2023-06-05 12:36:17 +0200

    added metrics and apiservices type

--- lib/check/checkKubernetes.py
+++ lib/check/checkKubernetes.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from kubernetes_asyncio import client, config
 from kubernetes_asyncio.client.api_client import ApiClient
@@ -96,27 +97,55 @@ class CheckKubernetes(CheckBase):
             await config.load_kube_config()
 
         async with ApiClient() as api:
-
-            cust = client.CustomObjectsApi(api)
-            res = await cust.list_cluster_custom_object(
-                'metrics.k8s.io', 'v1beta1', 'nodes')
-            node_metrics = {
-                i['metadata']['name']: i
-                for i in res['items']
-            }
-
-            res = await cust.list_cluster_custom_object(
-                'metrics.k8s.io', 'v1beta1', 'pods')
-            metrics = {
-                (
-                    i['metadata']['namespace'],
-                    i['metadata']['name']
-                ): {
-                    c['name']: c
-                    for c in i['containers']
+            cust = client.ApiregistrationV1Api(api)
+            res = await cust.list_api_service()
+            apis = {
+                i.metadata.name:
+                {
+                    'name': i.metadata.name,
+                    'available': any(
+                        c.type == 'Available' and c.status
+                        for c in i.status.conditions
+                    ),
+                    'service': i.spec.service and (
+                        f'{i.spec.service.namespace}/{i.spec.service.name}'
+                    ),
+                    'creation_timestamp':
+                    int(i.metadata.creation_timestamp.timestamp()),
                 }
-                for i in res['items']
+                for i in res.items
             }
+
+            if 'v1beta1.metrics.k8s.io' in apis:
+                cust = client.CustomObjectsApi(api)
+                res = await cust.list_cluster_custom_object(
+                    'metrics.k8s.io', 'v1beta1', 'nodes')
+                node_metrics = {
+                    i['metadata']['name']: i
+                    for i in res['items']
+                }
+
+                res = await cust.list_cluster_custom_object(
+                    'metrics.k8s.io', 'v1beta1', 'pods')
+                metrics = {
+                    (
+                        i['metadata']['namespace'],
+                        i['metadata']['name']
+                    ): {
+                        c['name']: c
+                        for c in i['containers']
+                    }
+                    for i in res['items']
+                }
+            else:
+                logging.warning(
+                    "API Service `v1beta1.metrics.k8s.io` is not available; "
+                    "make sure the metrics server is installed and check if "
+                    "the api service is running using `kubectl get "
+                    "apiservices`; see: "
+                    "https://github.com/kubernetes-sigs/metrics-server")
+                node_metrics = {}
+                metrics = {}
 
             v1 = client.CoreV1Api(api)
             res = await v1.list_namespace()
@@ -166,6 +195,15 @@ class CheckKubernetes(CheckBase):
                     'pod_name': i.metadata.name,
                     'creation_timestamp':
                     int(i.metadata.creation_timestamp.timestamp()),
+                    'containers': len(i.status.container_statuses),
+                    'ready_containers': sum(
+                        cs.ready
+                        for cs in i.status.container_statuses
+                    ),
+                    'restarts': sum(
+                        cs.restart_count
+                        for cs in i.status.container_statuses
+                    ),
                     **on_pod_metrics(i, metrics)
                 }
                 for i in res.items
@@ -185,6 +223,11 @@ class CheckKubernetes(CheckBase):
                     dfmt(c.resources.requests.get('cpu'), True),
                     'requests_memory': c.resources.requests and
                     dfmt(c.resources.requests.get('memory')),
+                    'restarts': sum(
+                        cs.restart_count
+                        for cs in i.status.container_statuses
+                        if cs.name == c.name
+                    ),
                     **on_container_metrics(i, c, metrics)
                 }
                 for i in res.items
@@ -193,6 +236,7 @@ class CheckKubernetes(CheckBase):
             ]
 
         return {
+            'apiservices': tuple(apis.values()),
             'namespaces': namespaces,
             'nodes': nodes,
             'pods': pods,
