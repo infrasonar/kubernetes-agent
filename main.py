3fd0eedda234bb363f46b6aebdcb1e88b12b571b
Jeroen van der Heijden <jeroen@cesbit.com>
2024-09-25 15:57:24 +0200

    main

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ from lib.check.checkKubernetes import CheckKubernetes
 from lib.version import __version__ as version
 
 
-async def test():
-
-    check_data = \
-        await asyncio.wait_for(CheckKubernetes.run(), timeout=90)
-    import json
-
-    print(json.dumps(check_data["pods"], indent=4))
-
-
-
 if __name__ == '__main__':
     checks = [CheckKubernetes]
-
-    import asyncio, os
-    asyncio.run(test())
-
-
-    # Agent('kubernetes', version).start(checks, asset_kind='Kubernetes')
+    Agent('kubernetes', version).start(checks, asset_kind='Kubernetes')
